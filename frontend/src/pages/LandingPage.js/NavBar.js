72d4040ae42214dbd74a598baf129ba609e219cc
mutimutuje483 <mutimutujehope90@gmail.com>
2026-05-29 20:48:30 +0200

    navbar done

--- frontend/src/pages/LandingPage.js/NavBar.js
+++ frontend/src/pages/LandingPage.js/NavBar.js
@@ -1,4 +1,5 @@
-import Logo from "../../../src/assets/logo.png"
+import Logo from "../../../src/assets/logo.png";
+
 function Navbar() {
   return (
     <nav
@@ -6,33 +7,42 @@ function Navbar() {
       style={{
         backgroundColor: "#f4efeb",
         boxShadow: "0 4px 10px rgba(0,0,0,0.2)",
-        height: "95px",
       }}
     >
       <div className="container-fluid">
 
         {/* Logo */}
-  <div className="Logo">
-  <img src={Logo} alt="logo" />
-</div>
+        <div className="Logo">
+          <img
+            src={Logo}
+            alt="logo"
+            style={{
+              width: "80px",
+              height: "80px",
+              objectFit: "contain",
+            }}
+          />
+        </div>
 
-        {/* Toggle */}
+        {/* Mobile Toggle */}
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarNav"
+  aria-controls="navbarNav"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
         {/* Nav Links */}
         <div
           className="collapse navbar-collapse justify-content-center"
           id="navbarNav"
         >
           <ul
-            className="navbar-nav gap-5"
+            className="navbar-nav gap-lg-5 text-center"
             style={{
               fontSize: "20px",
               fontWeight: "700",
@@ -40,27 +50,19 @@ function Navbar() {
             }}
           >
             <li className="nav-item">
-              <a
-                className="nav-link active"
-                href="#"
-                style={{
-                  color: "#2b1b1b",
-                  borderBottom: "3px solid black",
-                  width: "fit-content",
-                }}
-              >
+              <a className="nav-link nav-custom" href="#">
                 Home
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#" style={{ color: "#2b1b1b" }}>
+              <a className="nav-link nav-custom" href="#">
                 Our Menu
               </a>
             </li>
 
             <li className="nav-item">
-              <a className="nav-link" href="#" style={{ color: "#2b1b1b" }}>
+              <a className="nav-link nav-custom" href="#">
                 About
               </a>
             </li>
@@ -69,7 +71,7 @@ function Navbar() {
 
         {/* Button */}
         <button
-          className="btn px-4 py-2"
+          className="btn px-4 py-2 d-none d-lg-block"
           style={{
             backgroundColor: "#9c8585",
             color: "black",
@@ -82,6 +84,47 @@ function Navbar() {
           Get Started
         </button>
       </div>
+
+      {/* Internal CSS */}
+      <style>
+        {`
+          .nav-custom {
+            color: #2b1b1b;
+            position: relative;
+            transition: 0.3s ease;
+          }
+
+          .nav-custom::after {
+            content: "";
+            position: absolute;
+            left: 0;
+            bottom: -5px;
+            width: 0%;
+            height: 3px;
+            background: black;
+            transition: 0.3s ease;
+          }
+
+          .nav-custom:hover::after {
+            width: 100%;
+          }
+
+          .active-link::after {
+            width: 100%;
+          }
+
+          @media (max-width: 991px) {
+            .navbar-nav {
+              padding-top: 20px;
+              gap: 20px;
+            }
+
+            .nav-custom {
+              display: inline-block;
+            }
+          }
+        `}
+      </style>
     </nav>
   );
 }
