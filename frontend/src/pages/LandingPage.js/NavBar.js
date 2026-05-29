f97d9998e18428f6c0918f65a38b1cf81046ba87
mutimutuje483 <mutimutujehope90@gmail.com>
2026-05-29 20:38:56 +0200

    navbar

--- frontend/src/pages/LandingPage.js/NavBar.js
+++ frontend/src/pages/LandingPage.js/NavBar.js
@@ -1,30 +1,89 @@
-import "./landing.css";
-function Navbar(){
-    return(
-    <nav className="navbar navbar-expand-lg">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Our Menu</a>
-        </li>
-          <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
+import Logo from "../../../src/assets/logo.png"
+function Navbar() {
+  return (
+    <nav
+      className="navbar navbar-expand-lg px-4"
+      style={{
+        backgroundColor: "#f4efeb",
+        boxShadow: "0 4px 10px rgba(0,0,0,0.2)",
+        height: "95px",
+      }}
+    >
+      <div className="container-fluid">
 
-      </ul>
-        <button className="btn btn-outline-success" type="submit">Get</button>
-      
-    </div>
-  </div>
-</nav>
-);
+        {/* Logo */}
+  <div className="Logo">
+  <img src={Logo} alt="logo" />
+</div>
+
+        {/* Toggle */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Nav Links */}
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
+          <ul
+            className="navbar-nav gap-5"
+            style={{
+              fontSize: "20px",
+              fontWeight: "700",
+              fontFamily: "serif",
+            }}
+          >
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                href="#"
+                style={{
+                  color: "#2b1b1b",
+                  borderBottom: "3px solid black",
+                  width: "fit-content",
+                }}
+              >
+                Home
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#" style={{ color: "#2b1b1b" }}>
+                Our Menu
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#" style={{ color: "#2b1b1b" }}>
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Button */}
+        <button
+          className="btn px-4 py-2"
+          style={{
+            backgroundColor: "#9c8585",
+            color: "black",
+            fontWeight: "700",
+            borderRadius: "6px",
+            border: "none",
+            fontFamily: "serif",
+          }}
+        >
+          Get Started
+        </button>
+      </div>
+    </nav>
+  );
 }
-    export default Navbar;
+
+export default Navbar;
