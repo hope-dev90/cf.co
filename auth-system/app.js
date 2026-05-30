7a255fcbe68abb318911ae8b4662a1653995cbce
mutimutuje483 <mutimutujehope90@gmail.com>
2026-05-30 11:47:01 +0200

    lets push it is done

--- auth-system/app.js
+++ auth-system/app.js
@@ -2,6 +2,11 @@ import authRouter from "./routes/authRoutes.js";
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -9,13 +14,14 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({
     success: false,
-    message: "Route not found"
+    message: "Route not found",
   });
 });
 
@@ -23,7 +29,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
     success: false,
-    message: "Internal server error"
+    message: "Internal server error",
   });
 });
 
